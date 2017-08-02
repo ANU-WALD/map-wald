488e4989efd383d247958def508d7d2d0f453f1f
Joel Rahman <joel@flowmatters.com.au>
2017-08-02 21:08:06 +1000

    Basic string interpolation service

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ import { SamplePipe } from './src/sample.pipe';
 import { MapViewParameterService } from './src/map-view.service';
 import { WMSService } from './src/wms.service';
 import {GeocodingService} from './src/geocoding.service';
-
+import {InterpolationService} from './src/interpolation.service';
 import { ProjectionService } from './src/projection.service';
 import { WMSLayerComponent } from './src/wms-layer/wms-layer.component';
 import { MapLegendComponent } from './src/map-legend/map-legend.component';
@@ -21,11 +21,19 @@ export * from './src/sample.directive';
 export * from './src/sample.pipe';
 export * from './src/wms.service';
 export * from './src/projection.service';
-//export * from './src/csv.service';
 export * from './src/map-view.service';
 export * from './src/geocoding.service';
+export * from './src/interpolation.service';
 export * from './src/wms-layer/wms-layer.component';
 export * from './src/map-legend/map-legend.component';
+
+const services = [
+  WMSService,
+  MapViewParameterService,
+  ProjectionService,
+  GeocodingService,
+  InterpolationService
+];
 
 @NgModule({
   imports: [
@@ -49,25 +57,13 @@ export * from './src/map-legend/map-legend.component';
     SampleDirective,
     SamplePipe
   ],
-  providers: [
-//    CSVService,
-    WMSService,
-    MapViewParameterService,
-    ProjectionService,
-    GeocodingService
-  ]
+  providers: services
 })
 export class MapWaldModule {
   static forRoot(moduleInitialisation:any): ModuleWithProviders {
     return {
       ngModule: MapWaldModule,
-      providers: [
-//        CSVService,
-        WMSService,
-        MapViewParameterService,
-        ProjectionService,
-        GeocodingService
-      ]
+      providers: services
     };
   }
 }
