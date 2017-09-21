9be53ba41de762bf328b7de1c19f71de538f38dd
joel <joel@flowmatters.com.au>
2017-09-21 15:32:08 +1000

    Placeholder component for selecting base map style

---
+++ src/base-map-selection/base-map-selection.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+
+
+declare var Plotly: any;
+
+@Component({
+  selector: 'base-map-selection',
+  templateUrl: './base-map-selection.component.html',
+  styleUrls: ['./base-map-selection.component.scss']
+})
+export class BaseMapSelectionComponent implements AfterViewInit  {
+
+  constructor(){
+
+  }
+
+  ngAfterViewInit(){
+
+  }
+}
