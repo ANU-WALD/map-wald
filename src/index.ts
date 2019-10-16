1435be172950021ac4a89d92615a4aa9964fb23b
Joel Rahman <joel@flowmatters.com.au>
2019-10-16 11:37:34 +1100

    Restructure to just contain core, non-visual components

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,61 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {HttpClientModule} from '@angular/common/http';
-import {NgPipesModule} from 'ngx-pipes';
-import { AgmCoreModule } from '@agm/core';
-
 import { CommonModule } from '@angular/common';
-import { MapWaldCoreModule } from 'map-wald/core';
-import { MapWaldBootstrapModule } from 'map-wald/visual';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-// export * from '../core/src/index-core';
-// export * from '../visual/src/index-bootstrap';
+import { TreeFilterService } from './tree-filter.service';
+import { PaletteService } from './palette.service';
+import { TimeUtilsService } from './time-utils.service';
+import { StaticDataService } from './static-data.service';
+import { MetadataService } from './metadata.service';
+import { OpendapService } from './opendap.service';
+import { TimeseriesService } from './timeseries.service';
+import { PointSelectionService } from './point-selection.service';
+import { AvailableDatesService } from './available-dates.service';
+import { CatalogService } from './catalog.service';
+import { MapViewParameterService } from './map-view.service';
+import { WMSService } from './wms.service';
+import { ProjectionService } from './projection.service';
+
+export * from './data/catalog';
+export * from './data/mapped-layer';
+export * from './data/tree';
+export * from './data/bounds';
+export * from './data/actions';
+
+export * from './wms.service';
+export * from './projection.service';
+export * from './map-view.service';
+export * from './interpolation.service';
+export * from './available-dates.service';
+export * from './point-selection.service';
+export * from './metadata.service';
+export * from './catalog.service';
+export * from './palette.service';
+export * from './static-data.service';
+export * from './opendap.service';
+export * from './timeseries.service';
+export * from './time-utils.service';
+export * from './tree-filter.service';
+
+export * from './parsing/csv';
+
+const services = [
+  //$serviceList
+  AvailableDatesService,
+  PointSelectionService,
+  TimeseriesService,
+  StaticDataService,
+  MetadataService,
+  OpendapService,
+  PaletteService,
+  TimeUtilsService,
+  WMSService,
+  MapViewParameterService,
+  ProjectionService,
+  CatalogService,
+  TreeFilterService
+];
 
 //import { CSVService } from './src/csv.service';
 
@@ -18,29 +63,23 @@ import { MapWaldBootstrapModule } from 'map-wald/visual';
 
 //$exportList
 
-export * from './parsing/csv';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    AgmCoreModule,
-    HttpClientModule,
-    NgPipesModule,
-    NgbModule,
-    MapWaldBootstrapModule,
-    MapWaldCoreModule
+    HttpClientModule
   ],
   declarations: [],
   exports: [],
-  providers: []
+  providers: services
 })
-export class MapWaldModule {
-  // static forRoot(moduleInitialisation:any): ModuleWithProviders {
-  //   return {
-  //     ngModule: MapWaldModule,
-  //     providers: []
-  //   };
-  // }
+export class MapWaldCoreModule {
+  static forRoot(moduleInitialisation: any): ModuleWithProviders {
+    return {
+      ngModule: MapWaldCoreModule,
+      providers: services
+    };
+  }
 }
 
